c909fc70dea4b7f54baa270e9579d4ba0e646f85
Alex Schouls <alex_schouls@live.com>
2020-12-12 11:09:56 +0100

    Initial setup for retrieving 500 comments from an external source

---
+++ Cache.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Cache.Console
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Hello cache");
+            using IHost host = CreateHostBuilder(args).Build();
+            var cacheService = host.Services.GetRequiredService<CommentService>();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            System.Console.WriteLine("Starting timer...");
+            await cacheService.Execute();
+            System.Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms!");
+            await host.RunAsync();
+            System.Console.WriteLine("Goodbye cache");
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddStackExchangeRedisCache(options => options.Configuration = "localhost");
+
+                    services.AddTransient<CommentService>();
+                });
+    }
+}
---
+++ tmp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Cache.Console
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello cache");
+            using IHost host = CreateHostBuilder(args).Build();
+            var cacheService = host.Services.GetRequiredService<CommentService>();
+
+            await cacheService.Execute();
+            await host.RunAsync();
+            Console.WriteLine("Goodbye cache");
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddStackExchangeRedisCache(options => options.Configuration = "localhost");
+
+                    services.AddTransient<CommentService>();
+                });
+    }
+}
