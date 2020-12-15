86cdef55bffba3d4601cf0d656b66d0efcb2f992
Alex Schouls <alex_schouls@live.com>
2020-12-15 09:46:46 +0100

    Added first version of bootstrapper

---
+++ src/DCP.Bootstrapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace DCP.Bootstrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Distributed Cache Playground | Bootstrapper");
+
+            var applicationProcess = new Process();
+            var applicationProjectFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, 
+                                                                        @"..\..\..\..\DCP.Application"));
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "run -p DCP.Application.csproj",
+                WorkingDirectory = applicationProjectFolder,
+                UseShellExecute = true
+            };
+            applicationProcess.StartInfo = processInfo;
+            applicationProcess.Start();
+
+            WriteLine("The bootstrapper has finished its work, press any key to exit...");
+            ReadKey();
+        }
+    }
+}
